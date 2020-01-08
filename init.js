e73374ec7864eb216f1556e594f5e016f951a484
gudwnsgur <gudwnsgur12@naver.com>
2020-01-08 18:33:07 +0900

    Using webpack

--- init.js
+++ init.js
@@ -6,7 +6,7 @@ dotenv.config();
 import "./models/Video";
 import "./models/Comment";
 
-const PORT = process.env.PORT || 4003;
+const PORT = process.env.PORT || 4005;
 
 function handleListening() {
     console.log(`Listening on: http://localhost:${PORT}`);
