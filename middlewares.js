e500f98d522fe20c2d830124a61862e796a6cb7e
gudwnsgur <gudwnsgur12@naver.com>
2020-01-08 18:51:39 +0900

    styling of front-end

--- middlewares.js
+++ middlewares.js
@@ -7,7 +7,7 @@ export const localsMiddleware = function (req, res, next) {
     res.locals.siteName = "WeTube";
     res.locals.routes = routes;
     res.locals.user = {
-        isAuthenticated : true,
+        isAuthenticated : false,
         id : 1
     }
     // res.locals.변수명
